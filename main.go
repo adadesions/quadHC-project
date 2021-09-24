648cc56b809888c83fd7e519f5dd2405593ad99b
adadesions <adadesions@gmail.com>
2021-09-24 15:07:58 +0700

    View output from webview and merge server with in main file

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"image"
 	"image/color"
 	"image/png"
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/webview/webview"
 )
 
 func cvtToGray(img image.Image) *image.Gray {
@@ -54,18 +58,52 @@ func ImReadPNG(imgPath string) (image.Image, error) {
 	return img, nil
 }
 
+func display(imgPath string) {
+	debug := true
+	w := webview.New(debug)
+	defer w.Destroy()
+	baseURL := "http://localhost"
+	port := "8100"
+
+	w.SetTitle("QuadHC Experimental")
+	w.SetSize(800, 600, webview.HintNone)
+	w.Navigate(fmt.Sprintf("%s:%s/images/%s", baseURL, port, imgPath))
+	w.Run()
+}
+
+func startServer() {
+	port := flag.String("p", "8100", "port to serve on")
+	directory := flag.String("d", ".", "the directory of static file to host")
+	flag.Parse()
+
+	http.Handle("/", http.FileServer(http.Dir(*directory)))
+
+	log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
+}
+
 func main() {
-	img, err := ImReadPNG("images/sample1.png")
+	go startServer()
+	// img, err := ImReadPNG("images/sample1.png")
+
+	// imgFile, err := os.Open("data/kaggle_3m/TCGA_CS_4941_19960909/TCGA_CS_4941_19960909_1.tif")
+	imgFile, err := os.Open("images/sample1.png")
 	if err != nil {
 		log.Fatalf("Error occured during reading an image file: %v", err)
 	}
+	defer imgFile.Close()
+	imgFile.Seek(0, 0)
+
+	imgTiff, extension, err := image.Decode(imgFile)
 
 	if err != nil {
 		log.Fatalf("Error can't decode image: %v\n", err)
 	}
 
-	canvas := cvtToGray(img)
+	fmt.Printf("Img extension: %v\n", extension)
+
+	canvas := cvtToGray(imgTiff)
 	saveToPNG("images/output.png", canvas)
 	info(canvas, false)
-
+	display("output.png")
 }
